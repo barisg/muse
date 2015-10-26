0732161a614d7bfd1add801d61b9dc669a3e6e54
Barisg <baris-git@gmail.com>
2015-10-26 00:43:22 -0700

    init

---
+++ hello.py
@@ -0,0 +1,57 @@
+from flask import Flask, url_for, render_template, request
+from flask import make_response
+
+app = Flask(__name__)
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)    
+
+@app.route('/')
+def index():
+    return 'Hello World'
+    
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    return 'User %s' % username
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    # show the post with the given id, the id is an integer
+    return 'Post %d' % post_id
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if valid_login(request.form['username'],
+                       request.form['password']):
+            return log_the_user_in(request.form['username'])
+        else:
+            error = 'Invalid username/password'
+    # the code below is executed if the request method
+    # was GET or the credentials were invalid
+    return render_template('login.html', error=error)
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        f = request.files['the_file']
+        f.save('/var/www/uploads/uploaded_file.txt')
+            
+#@app.route('/login')
+#def login(): pass
+
+@app.route('/user/<username>')
+def profile(username): pass
+
+with app.test_request_context():
+    print url_for('index')
+    print url_for('login')
+    print url_for('login', next='/')
+    print url_for('profile', username='John Doe')
+
+if __name__ == '__main__':
+    app.run(debug=True)
